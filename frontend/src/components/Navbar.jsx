49c762b97d6592bd5c9cc948124b118f5f975927
EmmaBeni <lecbeni@mail.austral.edu.ar>
2026-02-25 00:01:38 -0300

    navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+export function Header() {
+    return (
+        <header style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            zIndex: 9999,
+            width: '100%',
+            height: '64px',
+            backgroundColor: '#800020'
+        }}>
+        </header>
+    )
+}
+
+//no tiene tailwind, uso css puro
--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,5 +1,14 @@
+import {Header} from "../components/Navbar.jsx";
+
 function Landing() {
-    return <h1>Cinefilia</h1>;
+    return (
+        <>
+        <Header/>
+
+        <h1 style={{ fontFamily: "'Allura', cursive", color: 'black'}}>Cinefilia</h1>
+        </>
+    );
 }
 
 export default Landing;
+//  <> y </> encueven varios elementos in agregar otro div, no pueden quedar sueltos
